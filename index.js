5f5bd1a717abf10e2f67cd88e8d753a004d4bac1
kaynoi <sekisuigmd@gmail.com>
2024-12-03 11:40:16 +0900

    indexfixとpackage

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
-
-'use strict';
-
-const express = require('express');
-const line = require('@line/bot-sdk');
-const PORT = process.env.PORT || 3000;
-
-const config = {
-    channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-};
-
+const https = require("https");
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
-app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req.body.events);
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return; 
-    }
-
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result));
+app.get("/", (req, res) => {
+  res.sendStatus(200);
 });
 
-const client = new line.Client(config);
+app.post("/webhook", function (req, res) {
+  res.send("HTTP POST request sent to the webhook URL!");
+});
 
-async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
-}
-
-app.listen(PORT);
-console.log(`Server running at ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
